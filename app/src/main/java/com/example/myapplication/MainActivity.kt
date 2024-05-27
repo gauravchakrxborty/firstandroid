bf4482303a1ba3b3b6847323d5644dd1c1e8cf8b
gaurav <gauravchakrxborty@gmail.com>
2024-05-27 10:15:17 +0530

    First Commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello World $name!",
+            text = "Hello World My Name is Gaurav $name!",
             modifier = modifier
     )
 }
